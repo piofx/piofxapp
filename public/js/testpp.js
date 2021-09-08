f54a3ef418fbf87618b2ffd0f9737fce9b77ccc7
Krishna Teja <founder@packetprep.com>
2021-09-08 16:28:12 +0530

    some changes

--- public/js/testpp.js
+++ public/js/testpp.js
@@ -1,6 +1,7 @@
+
 $(function(){
 
-    var prefix_url = 'https://xplore.co.in/';
+    var prefix_url = 'https://xplore.in.net/';
 
     if($(".test-container").length){
 
@@ -27,11 +28,127 @@ $(function(){
                 url = url+'&test_score=1';
 
             $.get(url,function(data){
-                container.html(data);
+                $questions = JSON.parse(data);
+                $questions.forEach(function(d,i){
+                    console.log(d);
+                    ele = "<div class='row no-gutters q_"+slug+"_"+i+"' data-answer='"+d.answer+"'>\
+                            <div class='col-2 col-md-1'><div class=' bg-soft-info  py-1 px-1 text-center rounded'>"
+                            +(i+1)+
+                            "</div></div>\
+                            <div class='col-10 col-md-11'><div class='ml-3'>"
+                            +d.question+
+                            "</div></div>\
+                            </div>"+
+                            "<div class='mb-4'><div class='row'>\
+                                <div class='col-12 col-md-6'>\
+                                    <div class='row'>\
+                                        <div class='col-3 mb-2'>\
+                                            <div class='bg-soft-warning py-1 px-1 text-center rounded'>\
+                                            <input class='form-check-input mt-2' type='radio' name='q_"+slug+"_"+i+"' data-qno='"+i+"' data-slug='"+slug+"' value='a' >\
+                                            A\
+                                            </div>\
+                                        </div>\
+                                        <div class='col-9'>"
+                                        + d.a +
+                                        "</div>\
+                                    </div>\
+                                </div>\
+                                <div class='col-12 col-md-6'>\
+                                    <div class='row'>\
+                                        <div class='col-3 mb-2'>\
+                                            <div class='bg-soft-warning py-1 px-1 text-center rounded'>\
+                                            <input class='form-check-input mt-2' type='radio' name='q_"+slug+"_"+i+"' data-qno='"+i+"' data-slug='"+slug+"' value='b' >\
+                                            B\
+                                            </div>\
+                                        </div>\
+                                        <div class='col-9'>"
+                                        + d.b +
+                                        "</div>\
+                                    </div>\
+                                </div>\
+                            </div>"+
+                            "<div class='row'>\
+                                <div class='col-12 col-md-6'>\
+                                    <div class='row'>\
+                                        <div class='col-3 mb-2'>\
+                                            <div class='bg-soft-warning py-1 px-1 text-center rounded'>\
+                                            <input class='form-check-input mt-2' type='radio' name='q_"+slug+"_"+i+"' data-qno='"+i+"' data-slug='"+slug+"' value='c' >\
+                                            C\
+                                            </div>\
+                                        </div>\
+                                        <div class='col-9'>"
+                                        + d.c +
+                                        "</div>\
+                                    </div>\
+                                </div>\
+                                <div class='col-12 col-md-6'>\
+                                    <div class='row'>\
+                                        <div class='col-3 mb-2'>\
+                                            <div class='bg-soft-warning py-1 px-1 text-center rounded'>\
+                                            <input class='form-check-input mt-2' type='radio' name='q_"+slug+"_"+i+"' data-qno='"+i+"' data-slug='"+slug+"' value='d' >\
+                                            D\
+                                            </div>\
+                                        </div>\
+                                        <div class='col-9'>"
+                                        + d.d +
+                                        "</div>\
+                                    </div>\
+                                </div>\
+                            </div><div class='text-danger  incorrect incorrect_"+slug+"_"+i+" ' style='display:none'>Nah! Option (<span class='text-primary text-bold ans_"+slug+"_"+i+" '>D</span>) is not the correct answer</div>\
+                            <div class='text-success correct correct_"+slug+"_"+i+" ' style='display:none'>Wow! you have picked the  correct option (<span class='text-primary text-bold ans_"+slug+"_"+i+" '>D</span>)</div></div>"
+
+                    container.append(ele);
+                    
+                });
+                //load mathjax dynamically
+                setTimeout(function(){
+                  var script = document.createElement("script");
+                  script.type = "text/javascript";
+                  script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";   // use the location of your MathJax
+
+                  var config = 'MathJax.Hub.Config({' +
+                                 'extensions: ["tex2jax.js"],' +
+                                 'jax: ["input/TeX","output/HTML-CSS"]' +
+                               '});' +
+                               'MathJax.Hub.Startup.onload();';
+
+                  if (window.opera) {script.innerHTML = config}
+                               else {script.text = config}
+
+                  document.getElementsByTagName("head")[0].appendChild(script);
+                console.log('Mathjax loaded');
+
+                    },500);
+                //console.log($questions);
+                //container.html(data);
             });    
         });
         
     }
+
+    $(document).on('change','.form-check-input',function(){
+        $qno = $(this).data('qno');
+        $slug = $(this).data('slug');
+        $response = $(this).val();
+        $ans = $('.q_'+$slug+'_'+$qno).data('answer');
+        $('.ans_'+$slug+'_'+$qno).html($response.toUpperCase());
+        $('.correct_'+$slug+'_'+$qno).hide();
+        $('.incorrect_'+$slug+'_'+$qno).hide();
+        if($ans.toUpperCase() == $response.toUpperCase())
+        {
+
+            $('.correct_'+$slug+'_'+$qno).show();
+            confetti({
+              particleCount: 100,
+              spread: 70,
+              origin: { y: 0.6 }
+            });
+
+        }else{
+            $('.incorrect_'+$slug+'_'+$qno).show();
+        }
+        
+    });
 
     $(document).on('click', '.ajaxtestsubmit', function(e) {
         var slug = $(this).data('test');
