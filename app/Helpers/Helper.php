bfc6e3718660c0a2e2d10c6bb75b8beb1137833b
shaadomanthra <shaadomanthra@yahoo.com>
2022-02-10 11:08:45 +0530

    some changes

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -78,11 +78,15 @@ if (!function_exists('updateMetaDescription')) {
 	function updateMetaDescription($description){
 		$content = request()->get('app.theme.prefix');
 
-        $buffer = preg_replace('/(<meta name="description" content=")(.*?)(">)/i', '$1' . $description . '$3', $content);
-        // $buffer =  preg_replace('/(<meta name="description" content=")(.*?)(">)/i','<meta name="description" content="'.$description.'" >', $content);
+		$pieces = explode('<meta name="description" content="',$content);
+		$buffer = $pieces[0].'<meta name="description" content="'.$description.$pieces[1];
 
-       $buffer = preg_replace('/(<meta property="og:description" content=")(.*?)(">)/i', '$1' . $description . '$3', $buffer);
-        request()->merge(['app.theme.prefix' => $buffer,]);
+        //$buffer = preg_replace('/(<meta name="description" content=")(.*?)(">)/i', '$1' . $description . '$3', $content);
+        // $buffer =  preg_replace('/(<meta name="description" content=")(.*?)(">)/i','<meta name="description" content="'.$description.'" >', $content);
+		$pieces = explode('<meta property="og:description" content="',$content);
+		$new_buffer = $pieces[0].'<meta property="og:description" content="'.$description.$pieces[1];
+       //$new_buffer = preg_replace('/(<meta property="og:description" content=")(.*?)(">)/i', '$1' . $description . '$3', $buffer);
+        request()->merge(['app.theme.prefix' => $new_buffer,]);
 	}
 }
 
