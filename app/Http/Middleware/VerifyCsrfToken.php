2c8179c666241ab14e76de20a836e9436694d28a
shaadomanthra <shaadomanthra@yahoo.com>
2022-10-25 13:03:24 +0530

    some changes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,5 +11,5 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [ 'api/*','calltrigger'];
+    protected $except = [ 'api/*','calltrigger','icalltrigger'];
 }
