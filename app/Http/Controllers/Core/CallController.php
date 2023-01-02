eb1ae88c4595bd14adafed6b7f7a3cc7f74f42a0
shaadomanthra <shaadomanthra@yahoo.com>
2023-01-02 22:50:52 +0530

    some changes

--- app/Http/Controllers/Core/CallController.php
+++ app/Http/Controllers/Core/CallController.php
@@ -188,7 +188,7 @@ class CallController extends Controller
         if(isset($data['notes']))
             $dd['remarks'] = $data['remarks'] = $data['notes'];
 
-        $data['name'] = $data['customer']['name'];
+        $call->name = $data['name'] = $data['customer']['name'];
         $data['phone'] = $data['customer']['phoneNumber'];
         $data['interaction_at'] = date('Y-m-d h:m:s',$data['createdAt']);
 
@@ -204,7 +204,7 @@ class CallController extends Controller
         if($data['demo_date']){
             $call->demo_date =  $data['demo_date'];
         }
-        $call->data =json_encode($data,JSON_PRETTY_PRINT);
+        $call->data =json_encode($dd,JSON_PRETTY_PRINT);
         
         $call->save();
        // $data['completed'] = 1;
