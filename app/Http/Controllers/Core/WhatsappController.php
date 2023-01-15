3cba2485ece39770abc0dc57fae5022c508e166d
shaadomanthra <shaadomanthra@yahoo.com>
2023-01-15 14:49:01 +0530

    some changes

--- app/Http/Controllers/Core/WhatsappController.php
+++ app/Http/Controllers/Core/WhatsappController.php
@@ -118,10 +118,13 @@ class WhatsappController extends Controller
         if(isset($d['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']))
         $text = $d['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
         $d['otp'] = -1;
-        $path = Storage::disk('public')->put('wadata/sample_2.json', json_encode($d));
+        $d['text'] = $text;
 
         $rem_str = 'rem_'.$phone.'_status';
         $status_str = Cache::get($rem_str);
+        $d['str'] = $status_str;
+        $path = Storage::disk('public')->put('wadata/sample_2.json', json_encode($d));
+
         if($text =='Generate OTP' && $status_str){
             $template = 'otp';
             $otp = substr($phone,-4);
