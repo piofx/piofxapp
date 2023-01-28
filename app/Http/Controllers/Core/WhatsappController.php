d5b5b5461b99cff4dfb2f9b42fb58ca619f73112
shaadomanthra <shaadomanthra@yahoo.com>
2023-01-28 18:50:04 +0530

    some changes

--- app/Http/Controllers/Core/WhatsappController.php
+++ app/Http/Controllers/Core/WhatsappController.php
@@ -86,7 +86,7 @@ class WhatsappController extends Controller
             
         }else if($show_2){
             $d = Storage::disk('public')->get('wadata/sample_2.json');
-            $d = json_encode($d, JSON_PRETTY_PRINT);
+            $d = json_encode(json_decode($d),JSON_PRETTY_PRINT);
             echo "<pre><code>";
             echo $d;
             echo "</code></pre>";
