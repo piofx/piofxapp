3bc3c4c8e8c10a4093d8acfb47100177874fa281
shaadomanthra <shaadomanthra@yahoo.com>
2023-01-29 11:13:45 +0530

    some changes

--- app/Http/Controllers/Core/WhatsappController.php
+++ app/Http/Controllers/Core/WhatsappController.php
@@ -256,10 +256,10 @@ class WhatsappController extends Controller
         $d['entry'][0]['email'] = 6;
         $path = Storage::disk('public')->put('wadata/sample.json', json_encode($d));
 
-        return true;
-        // if((strpos($email, '@') !== false) && (strpos($email, '.') !== false))
-        //     return true;
-        // else
-        //     return false;
+        
+        if((strpos($email, '@') !== false) && (strpos($email, '.') !== false))
+            return true;
+        else
+            return false;
     }
 }
