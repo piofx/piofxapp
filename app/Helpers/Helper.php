0f1a5566f023a1c74367eed5e7ad92d8dddb89e5
shaadomanthra <shaadomanthra@yahoo.com>
2023-03-03 15:11:48 +0530

    some changes

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -690,7 +690,7 @@ if (! function_exists('blog_image_upload')) {
 
 	if(!function_exists("zones")){
 		function zones(){
-			$data = ["Vijayawada","Visakhapatnam","Tirupati","Warangal","Guntur","Kakinada","Anantapur","Karimnagar","Khammam","Dilshuknagar","Secunderabad","Ameerpet","Kukatpally","Mehdipatnam","Narayanguda","Kompally","Uppal-Ghatkesar","Filmcity","Chevella","Ibrahimpatnam","Shamshabad","Vizianagaram","Kurnool","Eluru","Nellore","Rajamundry","Rajamundry","Bhimavaram","Ongole","Kadapa"];
+			$data = ["Vijayawada","Visakhapatnam","Tirupati","Warangal","Guntur","Kakinada","Anantapur","Karimnagar","Khammam","Dilshuknagar","Secunderabad","Ameerpet","Kukatpally","Mehdipatnam","Narayanguda","Kompally","Uppal-Ghatkesar","Filmcity","Chevella","Ibrahimpatnam","Shamshabad","Vizianagaram","Kurnool","Nellore","Ongole","Kadapa"];
 	        sort($data);
 	        return $data;
 		}
