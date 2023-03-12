0fe81755bff62094c9f158197987c9af85fcec8d
shaadomanthra <shaadomanthra@yahoo.com>
2023-03-12 17:37:42 +0530

    some changes

--- resources/views/auth/domains/fresherbot_com.blade.php
+++ resources/views/auth/domains/fresherbot_com.blade.php
@@ -218,6 +218,8 @@
                 <input type="hidden" name="agency_id" value="{{ request()->get('agency.id') }}">
                 <input type="hidden" name="client_id" value="{{ request()->get('client.id') }}">
                 <input type="hidden" name="redirect" value="{{ request()->get('redirect') }}">
+                <input type="hidden" name="wa_tracker" value="1">
+                <input type="hidden" name="phone_subscribe" value="1">
                 <input type="hidden" name="settings_utm_source" value="{{ request()->get('utm_source') }}">
                 <input type="hidden" name="settings_utm_campaign" value="{{ request()->get('utm_campaign') }}">
                 <input type="hidden" name="settings_utm_medium" value="{{ request()->get('utm_medium') }}">
